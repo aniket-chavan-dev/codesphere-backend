12a595414e98a7910e852f4133248de16f52d11e
aniket-chavan-dev <aniket.chavan.dev.18@gmail.com>
2026-01-01 20:20:24 +0530

    addes a view which push job into queue

--- runcode/views.py
+++ runcode/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -9,7 +8,7 @@ from json import JSONDecodeError
 from .models import Code
 from problems.models import Problems
 from .utility import convert_user_code_into_execution
-
+from codeexequeue.models import CodeQueue
 class RunPublicTestCases(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -29,33 +28,49 @@ class RunPublicTestCases(APIView):
             else :
                 code_instance.code = code
                 code_instance.save()
-            
+            #the code is converted into a format which can be executed with the test cases
             updated_code = convert_user_code_into_execution(code=code,test_cases=test_cases)
-            url = "https://codespherejudge-1.onrender.com/run/"
-            data = {
-                'code' : updated_code
+
+            job = CodeQueue.objects.create(
+                code = updated_code,
+                user_id = user.id,
+                problem_id = problem_id,
+            )
+
+            res = {
+                'status' : 'pending',
+                'jobid' : job.id
             }
-            print(updated_code)
-            try:
-                res = requests.post(url=url, data=json.dumps(data))
-                try:
-                    result_json = res.json()
-                except JSONDecodeError:
-                    return Response(
-                        {
-                            'error': 'Invalid JSON response from microservice',
-                            'response_text': res.text,
-                            'test_cases' : test_cases['test_cases']
-                        },
-                        status=status.HTTP_502_BAD_GATEWAY
-                    )
 
-                return Response({'result': result_json,'test_cases' : test_cases['test_cases']}, status=status.HTTP_200_OK)
+            return Response(res,status=status.HTTP_200_OK)
 
-            except requests.exceptions.RequestException as e:
-                return Response(
-                    {'error': str(e)},
-                    status=status.HTTP_503_SERVICE_UNAVAILABLE
-                )
+
+
+            # url = "https://codespherejudge-1.onrender.com/run/"
+            # data = {
+            #     'code' : updated_code
+            # }
+            # print(updated_code)
+            # try:
+            #     res = requests.post(url=url, data=json.dumps(data))
+            #     try:
+            #         result_json = res.json()
+            #     except JSONDecodeError:
+            #         return Response(
+            #             {
+            #                 'error': 'Invalid JSON response from microservice',
+            #                 'response_text': res.text,
+            #                 'test_cases' : test_cases['test_cases']
+            #             },
+            #             status=status.HTTP_502_BAD_GATEWAY
+            #         )
+
+            #     return Response({'result': result_json,'test_cases' : test_cases['test_cases']}, status=status.HTTP_200_OK)
+
+            # except requests.exceptions.RequestException as e:
+            #     return Response(
+            #         {'error': str(e)},
+            #         status=status.HTTP_503_SERVICE_UNAVAILABLE
+            #     )
         return Response({'msg' : 'method not allowed'},status=status.HTTP_400_BAD_REQUEST)
 
