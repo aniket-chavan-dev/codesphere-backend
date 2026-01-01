f84d3026db1172458bd59a6bb739928c5af61f11
aniket-chavan-dev <aniket.chavan.dev.18@gmail.com>
2026-01-01 20:11:19 +0530

    add a model CodeQueue

--- codeexequeue/models.py
+++ codeexequeue/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class CodeQueue(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('running', 'Running'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    ]
+
+    code = models.TextField()
+    user_id = models.IntegerField()
+    problem_id = models.IntegerField()
+    status = models.CharField(max_length=50, default='pending',choices=STATUS_CHOICES)  # e.g., pending, running, completed, failed
+    output = models.TextField(blank=True, null=True)
+    error = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"CodeQueue(id={self.id}, user_id={self.user_id}, problem_id={self.problem_id}, status={self.status})"
